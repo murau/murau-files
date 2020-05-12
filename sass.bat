e9105c5c367e37b27af7b373fbfa686af8a5435b
Murau - Plante Amor <desen@murau.com.br>
2020-05-12 14:31:52 -0300

    Testes

--- sass.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "C:\Users\Plante Amor\OneDrive\Development\murau-files"
-cmd /k "sass scss/bootstrap.scss:css/murau-bootstrap.min.js --style=compressed --watch"
-cmd /k "sass scss/bootstrap.scss:css/murau-bootstrap.js --watch"
-pause
